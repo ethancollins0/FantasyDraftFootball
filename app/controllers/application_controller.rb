06a3a5152f9d84892a863b5a519c6dc3e8113632
Ethan Collins <ethantcollins98@gmail.com>
2019-08-19 16:46:03 -0600

    sets up routes for view page and add page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
     def home
+        isLoggedIn?
+        @players = User.where(name: session[:name]).first.players
+    end
+
+    def add_player
+        isLoggedIn?
+    end
+
+    private
+
+    def isLoggedIn?
         if session[:name]
         else
             redirect_to '/login'
         end
     end
-    
+
     protect_from_forgery with: :exception
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'sessions/new'
   root 'application#home'
+  
   get '/', to: 'application#home'
+  get '/application/add_player'
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
